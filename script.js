51483655393b38fca6f1dc6173f7f489d8a2a6d4
bophancungung <bophancungung@gmail.com>
2021-08-05 19:53:01 +0700

    Fetching data

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-// alert('helllllooooo');
+// Unsplash API
+const count = 10;
+const apiKey = 'PTsARMWUcaJgihnpeAkLc8lJCl0QkFzX3ir2CmNVbmc';
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// Get photos from Unsplash API
+async function getPhotosFromAPI() {
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    console.log(data);
+  }
+  catch(error) {
+    console.log(error);
+  }
+}
+
+getPhotosFromAPI();
