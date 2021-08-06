bff536056b70236d2509b301246f84320e81034f
bophancungung <bophancungung@gmail.com>
2021-08-06 11:57:10 +0700

    finish

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 const imageContainer = document.getElementById('image-container');
 const loader = document.getElementById('loader');
 
+let isLoading = false;
+let imagesLoaded = 0;
+let totalImagesLoaded = 0;
 let photosArray = [];
 
 // Unsplash API
-const count = 10;
+const count = 30;
 const apiKey = 'PTsARMWUcaJgihnpeAkLc8lJCl0QkFzX3ir2CmNVbmc';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// Check if all images were loaded
+function checkImagesLoaded() {
+  console.log('image loaded');
+  imagesLoaded++;
+  console.log('imgaes loaded:', imagesLoaded);
+  if (imagesLoaded === totalImagesLoaded) {
+    isLoading = true;
+    loader.hidden = true;
+    console.log('is loading:', isLoading);
+  }
+}
 
 // Helper function to set attributes on DOM elements
 function setAttributes(element, attributes) {
@@ -17,6 +32,9 @@ function setAttributes(element, attributes) {
 
 // Create elements for links & photos, add to DOM
 function displayPhotos() {
+  imagesLoaded = 0;
+  totalImagesLoaded = photosArray.length;
+  console.log('total images loaded:', totalImagesLoaded);
   // Run function for each object in photosArray
   photosArray.forEach((photo) => {
     // Create <a> to link to Unsplash
@@ -32,6 +50,8 @@ function displayPhotos() {
       alt: photo.alt_description,
       title: photo.description
     });
+    // Event listener, check when each is finished loading
+    img.addEventListener('load', checkImagesLoaded);
     // Put <img> inside <a>, then put both inside imageContainer Element
     item.appendChild(img);
     imageContainer.appendChild(item);
@@ -49,5 +69,16 @@ async function getPhotosFromAPI() {
     console.log(error);
   }
 }
+
+// Check to see if scrolling near bottom of page, Load more photos
+window.addEventListener('scroll', () => {
+  const innerHeightScrollY = window.innerHeight + window.scrollY;
+  const beforeOffsetHeight = document.body.offsetHeight - 1000;
+  if (innerHeightScrollY >= beforeOffsetHeight && isLoading) {
+    isLoading = false;
+    getPhotosFromAPI();
+  }
+})
+
 
 getPhotosFromAPI();
